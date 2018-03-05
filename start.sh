f9ce6136807587cc2ffc03652bbeabf308096855
RockYuan <RockYuan@gmail.com>
2018-03-05 20:18:12 +0800

    fix for windows
    Permissions for '/app/ssh_id_file' are too open

--- start.sh
+++ start.sh
@@ -2,6 +2,12 @@
 
 if [ -z ${SSH_HOST+x} ]; then echo "Invalid argument to exit"; exit 128; fi
 
+if [ ! -f /app/ssh_id_file ]; then
+    echo "ssh_id_file not found!"; exit 12;
+fi
+
+chmod 400 /app/ssh_id_file
+
 ssh-keyscan $SSH_HOST >> ~/.ssh/known_hosts
 
 ssh -i $ID_FILE -nNT -o GatewayPorts=true -L $LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT $SSH_USER@$SSH_HOST
