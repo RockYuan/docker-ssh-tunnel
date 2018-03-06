9010d8780405d3aed17ded901c3a50da63386a5d
RockYuan <RockYuan@gmail.com>
2018-03-06 09:44:07 +0800

    fix for window
    Permissions 0555 for ssh_id_file are too open.

--- start.sh
+++ start.sh
@@ -2,12 +2,11 @@
 
 if [ -z ${SSH_HOST+x} ]; then echo "Invalid argument to exit"; exit 128; fi
 
-if [ ! -f /app/ssh_id_file ]; then
-    echo "ssh_id_file not found!"; exit 12;
-fi
+if [ ! -f /app/ssh.pem ]; then echo "ssh.pem not found!"; exit 12; fi
 
+cp /app/ssh.pem /app/ssh_id_file
 chmod 400 /app/ssh_id_file
 
 ssh-keyscan $SSH_HOST >> ~/.ssh/known_hosts
 
-ssh -i $ID_FILE -nNT -o GatewayPorts=true -L $LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT $SSH_USER@$SSH_HOST
+ssh -i /app/ssh_id_file -nNT -o GatewayPorts=true -L $LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT $SSH_USER@$SSH_HOST
